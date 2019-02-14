b64c1cc3ab220e4479ceeb5df69fab814d1be220
K. Adam White <kadamwhite@users.noreply.github.com>
2019-02-14 17:11:33 -0500

    Move manifest filename to manifest module; expose in index

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ module.exports = {
 	externals: require( './lib/externals' ),
 	helpers: require( './lib/helpers' ),
 	loaders: require( './lib/loaders' ),
+	manifest: require( './lib/manifest' ),
 	plugins: require( './lib/plugins' ),
 	presets: require( './lib/presets' ),
 };
