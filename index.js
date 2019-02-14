748288b02feb169da5ca7ce51f5a497d3887060d
K. Adam White <kadamwhite@users.noreply.github.com>
2019-02-14 16:35:54 -0500

    Expose new modules through index.js

--- index.js
+++ index.js
@@ -2,7 +2,11 @@
  * Expose public package API.
  */
 module.exports = {
+	/* eslint-disable global-require */
+	config: require( './lib/config' ),
 	externals: require( './lib/externals' ),
 	helpers: require( './lib/helpers' ),
 	loaders: require( './lib/loaders' ),
+	plugins: require( './lib/plugins' ),
+	presets: require( './lib/presets' ),
 };
