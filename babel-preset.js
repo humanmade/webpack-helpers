afbeb73fc138d2b71998a04a4056aaa3b570c3b5
Thorsten Frommen <info@tfrommen.de>
2019-07-07 21:18:38 +0200

    Use Babel plugin to support class properties

--- babel-preset.js
+++ babel-preset.js
@@ -1,6 +1,7 @@
 module.exports = {
 	presets: [ '@wordpress/default' ],
 	plugins: [
+		'@babel/plugin-proposal-class-properties',
 		[ 'transform-react-jsx', {
 			pragma: 'wp.element.createElement',
 		} ],
