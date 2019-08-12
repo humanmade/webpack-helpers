0af0aedb9af723e394d97df318b06c60b82112fb
tfrommen <info@tfrommen.de>
2019-08-12 09:44:25 +0200

    Fix filePath test for Windows

--- src/helpers/file-path.js
+++ src/helpers/file-path.js
@@ -1,3 +1,4 @@
+const process = require( 'process' );
 const { join } = require( 'path' );
 
 /**
--- src/helpers/file-path.test.js
+++ src/helpers/file-path.test.js
@@ -1,9 +1,29 @@
 const filePath = require( './file-path' );
 
+jest.mock( 'process', () => ( { cwd: () => 'cwd' } ) );
+
+expect.extend( {
+	toMatchFilePath( received, expected ) {
+		const pass = ( new RegExp( received.replace( /(\\|\/)/g, '(\\\\|\/)' ) ) ).test( expected );
+		if ( pass ) {
+			return {
+				message: () => `expected ${ received } not to match file path ${ expected }`,
+				pass: true,
+			};
+		}
+
+		return {
+			message: () => `expected ${ received } to match file path ${ expected }`,
+			pass: false,
+		};
+	},
+} );
+
 describe( 'helpers/file-path', () => {
 	it( 'properly generates a file system theme file path', () => {
-		expect( filePath() ).toBe( process.cwd() );
-		expect( filePath( 'themes/theme-name' ) ).toBe( `${ process.cwd() }/themes/theme-name` );
-		expect( filePath( 'themes', 'theme-name', 'style.css' ) ).toBe( `${ process.cwd() }/themes/theme-name/style.css` );
+		expect( filePath() ).toMatchFilePath( 'cwd' );
+		expect( filePath( 'themes/theme-name' ) ).toMatchFilePath( 'cwd/themes/theme-name' );
+		expect( filePath( 'themes\\theme-name' ) ).toMatchFilePath( 'cwd/themes/theme-name' );
+		expect( filePath( 'themes', 'theme-name', 'style.css' ) ).toMatchFilePath( 'cwd/themes/theme-name/style.css' );
 	} );
 } );
