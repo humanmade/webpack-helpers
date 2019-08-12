e1c53a0db752610bb0edce520f319b263f745ebf
tfrommen <info@tfrommen.de>
2019-08-12 19:01:08 +0200

    Fix ESLint errors

--- src/helpers/file-path.test.js
+++ src/helpers/file-path.test.js
@@ -3,8 +3,8 @@ const filePath = require( './file-path' );
 jest.mock( 'process', () => ( { cwd: () => 'cwd' } ) );
 
 expect.extend( {
-	toMatchFilePath( received, expected ) {
-		const pass = ( new RegExp( received.replace( /(\\|\/)/g, '(\\\\|\/)' ) ) ).test( expected );
+	toMatchFilePath: ( received, expected ) => {
+		const pass = ( new RegExp( received.replace( /(\\|\/)/g, '(\\\\|/)' ) ) ).test( expected );
 		if ( pass ) {
 			return {
 				message: () => `expected ${ received } not to match file path ${ expected }`,
