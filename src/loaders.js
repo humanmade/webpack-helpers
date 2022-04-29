e702595d180be74a63b1890bbf5a68221d37c860
K Adam White <kadamwhite@users.noreply.github.com>
2022-04-29 10:56:18 -0400

    Update src/loaders.js
    Co-authored-by: Thorsten Frommen <info@tfrommen.de>

--- src/loaders.js
+++ src/loaders.js
@@ -121,7 +121,7 @@ loaders.resource.defaults = {
 	// its runtime that would otherwise be processed through "file" loader.
 	// Also exclude `html` and `json` extensions so they get processed
 	// by webpacks internal loaders.
-	exclude: [ /^$/, /\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/ ],
+	exclude: [ /^$/, /\.(js|mjs|jsx|ts|tsx)$/, /\.html?$/, /\.json$/ ],
 	type: 'asset/resource',
 };
 
