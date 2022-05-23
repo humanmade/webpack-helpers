bcd10add2b9ac1ec45ac0d3cc757ac4918776140
K. Adam White <kadamwhite@users.noreply.github.com>
2022-05-23 11:57:09 -0400

    Permit filtering Terser defaults

--- src/plugins.js
+++ src/plugins.js
@@ -10,7 +10,7 @@ const SimpleBuildReportPlugin = require( 'simple-build-report-webpack-plugin' );
 const TerserPlugin = require( 'terser-webpack-plugin' );
 const CssMinimizerPlugin = require( 'css-minimizer-webpack-plugin' );
 
-const deepMerge = require( './helpers/deep-merge' );
+const { applyFilters } = require( './helpers/filters' );
 
 const isProfileMode = process.argv.includes( '--profile' );
 
@@ -187,43 +187,45 @@ module.exports = {
 	 * @returns {TerserPlugin} A configured TerserPlugin instance.
 	 */
 	terser: ( options = {} ) => new TerserPlugin( {
-		terserOptions: {
-			parse: {
-				// We want terser to parse ecma 8 code. However, we don't want it
-				// to apply any minification steps that turns valid ecma 5 code
-				// into invalid ecma 5 code. This is why the 'compress' and 'output'
-				// sections only apply transformations that are ecma 5 safe
-				// https://github.com/facebook/create-react-app/pull/4234
-				ecma: 8,
+		...applyFilters( 'plugin/terser/defaults', {
+			terserOptions: {
+				parse: {
+					// We want terser to parse ecma 8 code. However, we don't want it
+					// to apply any minification steps that turns valid ecma 5 code
+					// into invalid ecma 5 code. This is why the 'compress' and 'output'
+					// sections only apply transformations that are ecma 5 safe
+					// https://github.com/facebook/create-react-app/pull/4234
+					ecma: 8,
+				},
+				compress: {
+					ecma: 5,
+					warnings: false,
+					// Disabled because of an issue with Uglify breaking seemingly valid code:
+					// https://github.com/facebook/create-react-app/issues/2376
+					// Pending further investigation:
+					// https://github.com/mishoo/UglifyJS2/issues/2011
+					comparisons: false,
+					// Disabled because of an issue with Terser breaking valid code:
+					// https://github.com/facebook/create-react-app/issues/5250
+					// Pending further investigation:
+					// https://github.com/terser-js/terser/issues/120
+					inline: 2,
+				},
+				mangle: {
+					safari10: true,
+				},
+				// Added for profiling in devtools
+				keep_classnames: isProfileMode,
+				keep_fnames: isProfileMode,
+				output: {
+					ecma: 5,
+					comments: false,
+					// Turned on because emoji and regex is not minified properly using default
+					// https://github.com/facebook/create-react-app/issues/2488
+					ascii_only: true,
+				},
 			},
-			compress: {
-				ecma: 5,
-				warnings: false,
-				// Disabled because of an issue with Uglify breaking seemingly valid code:
-				// https://github.com/facebook/create-react-app/issues/2376
-				// Pending further investigation:
-				// https://github.com/mishoo/UglifyJS2/issues/2011
-				comparisons: false,
-				// Disabled because of an issue with Terser breaking valid code:
-				// https://github.com/facebook/create-react-app/issues/5250
-				// Pending further investigation:
-				// https://github.com/terser-js/terser/issues/120
-				inline: 2,
-			},
-			mangle: {
-				safari10: true,
-			},
-			// Added for profiling in devtools
-			keep_classnames: isProfileMode,
-			keep_fnames: isProfileMode,
-			output: {
-				ecma: 5,
-				comments: false,
-				// Turned on because emoji and regex is not minified properly using default
-				// https://github.com/facebook/create-react-app/issues/2488
-				ascii_only: true,
-			},
-		},
+		} ),
 		...options,
 	} ),
 };
