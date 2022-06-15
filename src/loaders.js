f4db3bdf5b0bc349eccdd38ee3325c6a569d2b72
K. Adam White <kadamwhite@users.noreply.github.com>
2022-06-15 12:40:59 +0200

    Improve inline documentation for filters

--- src/loaders.js
+++ src/loaders.js
@@ -24,11 +24,27 @@ const loaders = {};
  */
 const createLoaderFactory = loaderKey => {
 	return ( options = {}, config = null ) => {
-		// Generate the requested loader definition. Expose filter seams both
-		// to customize the defaults, and to alter the final rendered output.
+		/**
+		 * Generate the requested loader definition.
+		 *
+		 * Allows customization of the final rendered loader via the loaders/{loaderslug}
+		 * filter, which also receives the configuration passed to a preset factory if
+		 * the loader is invoked in the context of a preset.
+		 *
+		 * @hook loaders/{$loader_slug}
+		 * @param {Object}      loader   Loader definition object, after merging user-provided values with filtered defaults.
+		 * @param {Object|null} [config] Preset configuration object, if loader is called in context of preset.
+		 */
 		return applyFilters(
 			`loaders/${ loaderKey }`,
 			deepMerge(
+				/**
+				 * Filter the loader's default configuration.
+				 *
+				 * @hook loaders/{$loader_slug}/defaults
+				 * @param {Object}      options  Loader default options object.
+				 * @param {Object|null} [config] Preset configuration object, if loader is called in context of preset.
+				 */
 				applyFilters( `loaders/${ loaderKey }/defaults`, loaders[ loaderKey ].defaults, config ),
 				options
 			),
@@ -86,8 +102,20 @@ loaders.postcss.defaults = {
 	options: {
 		postcssOptions: {
 			ident: 'postcss',
+			/**
+			 * Filter the default PostCSS Plugins array.
+			 *
+			 * @hook loaders/postcss/plugins
+			 * @param {Array} plugins Array of PostCSS plugins.
+			 */
 			plugins: applyFilters( 'loaders/postcss/plugins', [
 				postcssFlexbugsFixes,
+				/**
+				 * Filter the default PostCSS Preset Env configuration.
+				 *
+				 * @hook loaders/postcss/preset-env
+				 * @param {Object} presetEnvConfig PostCSS Preset Env plugin configuration.
+				 */
 				postcssPresetEnv( applyFilters( 'loaders/postcss/preset-env', {
 					autoprefixer: {
 						flexbox: 'no-2009',
